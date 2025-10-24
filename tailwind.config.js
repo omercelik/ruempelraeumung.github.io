2849a3e4c99a2d766bcd8ed120f317378419f712
Omer Celik <ruempelraeumung@gmail.com>
2025-10-25 00:27:16 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*.md",
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./_services/**/*.md",
+    "./_guides/**/*.md",
+    "./_districts/**/*.md",
+    "./content/**/*.md",
+    "./assets/js/**/*.js",
+    "./_posts/**/*.md"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#0A2342",
+          light: "#2CA58D",
+          accent: "#FFD700"
+        },
+        "background-light": "#F5F5F5",
+        "background-dark": "#0f1723",
+        "text-light": "#333333",
+        "text-dark": "#F5F5F5"
+      },
+      fontFamily: {
+        display: ["Manrope", "sans-serif"]
+      }
+    }
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/container-queries")
+  ]
+};
